991e3343089e47714e7ce2cde191532bb61bd6c3
Diego Cardoso <98044200+diego-sbtlab@users.noreply.github.com>
2025-07-14 22:50:58 -0300

    add exception livraria

---
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -0,0 +1,10 @@
+public class LivroEdicaoOuro extends Livro {
+
+    public LivroEdicaoOuro(String titulo, String autor, double preco) {
+        super(titulo, autor, preco );
+    }
+
+    public double getPreco() {
+        return super.getPreco() +  super.getPreco() * 0.30;
+    }
+}
