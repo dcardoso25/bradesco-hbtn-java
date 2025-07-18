2366dc64a22a062e66090d9e0b4af44cc30069d3
Diego Cardoso <98044200+diego-sbtlab@users.noreply.github.com>
2025-07-17 22:25:10 -0300

    Create FileWritingExercise.java

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileWritingExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita o nome do arquivo
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            boolean finished = false;
+            while (scanner.hasNextLine() && !finished) {
+                String line = scanner.nextLine();
+                if (line.equals("sair")) {
+                    finished = true;
+                    System.out.println("Arquivo " + fileName + " foi salvo com sucesso!");
+                    continue;
+                }
+                if (!finished) {
+                    bw.write(line);
+                    bw.newLine();
+                }
+            }
+
+        } catch (java.lang.Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        scanner.close();
+
+
+
+    }
+}
