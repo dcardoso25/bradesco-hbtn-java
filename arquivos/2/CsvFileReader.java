77714417986b235264acaa4f2e87ace74ff0cd5f
Diego Cardoso <98044200+diego-sbtlab@users.noreply.github.com>
2025-07-18 22:54:02 -0300

    add arquivos 2 - csv fileReader

---
+++ arquivos/2/CsvFileReader.java
@@ -0,0 +1,26 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class CsvFileReader {
+    public static void main(String[] args) {
+        String filename = "funcionarios.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Nome,Idade,Departamento,Salarial
+                String[] data = line.split(",");
+                System.out.printf("Nome: %s\n", data[0]);
+                System.out.printf("Idade: %s\n", data[1]);
+                System.out.printf("Departamento: %s\n", data[2]);
+                System.out.printf("Salarial: %s\n", data[3]);
+                System.out.println("------------------------");
+            }
+            System.out.println("Leitura do arquivo concluída.\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
