428b296f0a8f3540cecd9f906160833c2b707dfd
Diego <36318050+dcardoso25@users.noreply.github.com>
2025-07-18 21:56:57 -0300

    add FileReadingExercise

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt";
+        try (
+                BufferedReader br = new BufferedReader(new FileReader(fileName))
+        ){
+            System.out.println("Conteudo do arquivo:\n");
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println();
+            System.out.println("Leitura do arquivo concluída.");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
