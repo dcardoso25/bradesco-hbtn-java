5894cccff28b5f7b3d3ae23c8787522973bd4229
Diego Cardoso <98044200+diego-sbtlab@users.noreply.github.com>
2025-07-21 17:15:22 -0300

    add - complexidade - Complexidade O(n) – Busca Linear

---
+++ complexidade/1/PessoasArray.java
@@ -0,0 +1,40 @@
+import java.lang.IllegalArgumentException;
+
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de buscaLinear
+    public void buscaLinear(String nome){
+        boolean encontrado = false;
+        System.out.println("Procurando pelo nome: " + nome);
+        for(int i=0;i<nomes.length;i++){
+            if(nomes[i].equals(nome)){
+                encontrado = true;
+                System.out.println("Nome pesquisado é "+nome+" que está na posição " + i);
+            } else {
+                System.out.println("Passando pelo indice: " + i);
+            }
+        }
+
+        if(!encontrado){
+            throw new IllegalArgumentException("O nome "+nome+" não se encontra no array de nomes");
+        }
+
+    }
+}
