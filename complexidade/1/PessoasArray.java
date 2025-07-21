71f14fd20712fe5aa5accfddbe185f12488be554
Diego Cardoso <98044200+diego-sbtlab@users.noreply.github.com>
2025-07-21 18:31:48 -0300

    Update PessoasArray.java

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -24,7 +24,7 @@ public class PessoasArray {
         boolean encontrado = false;
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
         for(int i=0;i<nomes.length;i++){
-            System.out.println("Passando pelo indice: " + i);
+            System.out.println("Passando pelo indice:" + i);
 
             if(nomes[i] == nome){
                 encontrado = true;
