3c7312d24327f8cf151d4565ed23a67350637655
Diego <36318050+dcardoso25@users.noreply.github.com>
2025-08-16 17:24:55 -0300

    add Java streams - 1. Relatório Produto com maior preço

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.Optional;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        List<Produto> produtos = pedido.getProdutos();
+        return produtos.stream()
+                .filter(e -> e.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        Optional<Produto> produtoMaiorPreco = produtos.stream()
+                .sorted(Comparator.comparingDouble(Produto::getPreco).reversed())
+                .findFirst();
+        return produtoMaiorPreco.get();
+    }
+}
