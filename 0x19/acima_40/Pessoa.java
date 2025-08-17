cedf42cc065f20ea42f2855658793ac5e161598f
Diego <36318050+dcardoso25@users.noreply.github.com>
2025-08-17 17:39:17 -0300

    add Java Stream - 2. Pessoas agrupadas por cargo e que possuam pelo menos 40 anos

---
+++ 0x19/acima_40/Pessoa.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+
+public class Pessoa {
+    // codigo, nome, cargo, idade e salario.
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format( new Locale("pt", "BR"), "[%d] %s %s %d R$ %.6f", this.codigo, this.nome, this.cargo, this.idade, this.salario);
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
