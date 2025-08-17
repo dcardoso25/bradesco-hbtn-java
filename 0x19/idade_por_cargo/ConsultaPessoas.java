742bad04aa835e03afa44fdf29c54ea5331eaf4a
Diego <36318050+dcardoso25@users.noreply.github.com>
2025-08-17 17:30:42 -0300

    add Java Streams - 1. Idades das pessoas agrupadas por cargo

---
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo));
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Pessoa::getCargo,
+                                Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+    }
+}
